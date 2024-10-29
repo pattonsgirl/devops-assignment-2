6b7a47544f0f7078ee3ccf5949f27cb376aa191a
pattonsgirl <pattonsgirl@gmail.com>
2024-10-29 14:10:01 -0400

    adding work order pro
    complete services w/ testing

---
+++ work-order-pro-service/src/test/java/com/wsu/workorderproservice/WorkOrderProServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.wsu.workorderproservice;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class WorkOrderProServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
